0fb49a757e8bb1c4e5a59de0b0e3c42b854c0dad
AriefDr <ariefdwir16@gmail.com>
2020-04-25 18:16:14 +0700

    fix path url

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,10 @@ document.getElementById('subs').addEventListener('click', () => {
     let emailSubs = document.getElementById('emailSubs')
     if (emailSubs.value == "" || emailSubs.value == null) {
         alert("email anda kosong")
-    } else if (emailSubs.value.length < 5 || emailSubs.value.length > 25) {
-        alert("charkater anda terlalu panjang melebihi 25")
+    } else if (emailSubs.value.length < 5) {
+        alert("charkater anda terlalu pendek kurang dari 5")
+    } else if (emailSubs.value.length > 25) {
+        alert("charkater anda terlalu panjang melebihi dari 25")
     } else if (emailSubs.value.indexOf("@") == -1) {
         alert("format email anda salah")
     } else {
